0b6856472a54b70f0fe0e898f31593527ae95540
200288btm-wq <200288btm@gmail.com>
2026-06-21 22:57:07 +0200

    Update TeachersPage.jsx

--- src/pages/TeachersPage.jsx
+++ src/pages/TeachersPage.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { supabase } from '../supabase'
 import { T, fmt, hashColor } from '../styles.jsx'
 import { Modal } from '../components/Modal'
